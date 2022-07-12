f918a42fcfa9d5c060dea0a38540541f66e09606
Casey <s3722194@student.rmit.edu.au>
2022-07-12 23:28:12 +1000

    game

---
+++ GameAI3/Assets/Scripts/DwarfController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DwarfController : MonoBehaviour
+{
+
+    private int speed = 5;
+    private Rigidbody2D body;
+    private Vector2 steering;
+    public Seek seek;
+
+    void Start(){
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void Update(){
+
+        
+        Vector2 targetPos = transform.position;
+        if (Input.GetKey(KeyCode.A))
+            targetPos -= Vector2.left;
+        if (Input.GetKey(KeyCode.D))
+            targetPos -= Vector2.right;
+        if (Input.GetKey(KeyCode.W))
+            targetPos -= Vector2.up;
+        if (Input.GetKey(KeyCode.S))
+            targetPos -= Vector2.down;
+
+        steering = seek.Movement(targetPos, transform.position, body.velocity, speed);
+
+        body.velocity = Vector2.ClampMagnitude(body.velocity + steering, speed);
+        transform.up = body.velocity.normalized;
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D col){
+        if(col.tag == "Goal"){
+            Destroy(this.gameObject);
+        }
+
+        if(col.tag == "River"){
+            speed = 2;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col){
+        if(col.tag == "River"){
+            speed = 5;
+        }
+    }
+}
